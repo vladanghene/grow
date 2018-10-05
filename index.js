f5cfb81f0b3214aa6a3574e4bfb9ec5eb99538d3
Vlad Anghene <vlad.anghene@gmail.com>
2018-10-05 20:16:30 +0300

    fixed state alterations and cleaned the code a bit

--- index.js
+++ index.js
@@ -16,15 +16,14 @@ var router = express.Router()
 
 state={
 	applicationConfig : {
-    		clientID: "2a56f63b-2904-4a09-9e02-9cf88138cffe",
-    		graphEndpoint: "https://graph.microsoft.com/beta"
+			clientID: process.env.APPID,
+			apppwd:process.env.APPPWD,
+			graphEndpoint: "https://graph.microsoft.com/beta",
+			redirectUrl:process.env.INSTANCE
 	},
 	auth: {
 		token: null,
 		clientcode: null,
-		appid:process.env.APPID,
-		apppwd:process.env.APPPWD,
-		redirectUrl:process.env.INSTANCE
 	},
 	filename: ""
 }
@@ -38,13 +37,13 @@ app.set('view engine', 'ejs');
 app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
 
 app.get('/', (req, res) => {res.redirect('getuser')});
-app.get('/echo', (req,res) =>{console.log("Echoed: ", req)});
+app.get('/getuser', (req,res) => getUserInfo(req,res));
 app.get('/gettoken', (req,res) => {
 	getToken(req,res)
 	});
-app.get('/getuser', (req,res) => getUserInfo(req,res));
 app.get('/token', (req,res) => {
-	state.applicationConfig.clientID=req.query.code;
+	console.log("got clientcode: ", req.query.code);
+	state.auth.clientcode = req.query.code;
 	res.redirect('/gettoken?code='+ req.query.code);
 });
 app.get('/download', (req,res) => {res.sendFile(path.join(__dirname, 'public', "document.pdf"))})
@@ -53,9 +52,10 @@ app.post('/upload', upload, (req,res) => uploadToDrive(req,res));
 app.get('/logout', (req, res)=>{state.auth.token=null;res.redirect('/')})
 
 function getAcceptance(res){
+	console.log("getting acceptance: ", state.applicationConfig);
 	res.redirect('https://login.microsoftonline.com/common/oauth2/v2.0/authorize?client_id='
-	+ state.auth.appid + '&scope=user.read Files.ReadWrite.All offline_access&redirect_uri='
-	+ state.auth.redirectUrl + "&response_type=code");
+	+ state.applicationConfig.clientID + '&scope=user.read Files.ReadWrite.All offline_access&redirect_uri='
+	+ state.applicationConfig.redirectUrl + "&response_type=code");
 }
 
 function buildTokenRequest (code) {
@@ -68,18 +68,19 @@ return	{
 		'Content-Type': 'application/x-www-form-urlencoded'
 	},
 	'form':{
-		'client_id':state.auth.appid,
-		'client_secret':state.auth.apppwd,
+		'client_id':this.state.applicationConfig.clientID,
+		'client_secret':this.state.applicationConfig.apppwd,
 		'code':code,
-		'redirect_uri':state.auth.redirectUrl,
+		'redirect_uri':this.state.applicationConfig.redirectUrl,
 		'grant_type':'authorization_code',
 		'scope':'user.read Files.ReadWrite Files.ReadWrite.All',
 	}
 }}
 
 function getUserOptions(tkn) {
-	return {'uri':this.state.applicationConfig.graphEndpoint+"/me",
-	'headers':{
+	uri=state.applicationConfig.graphEndpoint+"/me";
+	return {'uri':uri,
+		'headers':{
 		'Authorization':'Bearer '+tkn,
 		'User-Agent':'request',
 		'Content-Type': 'application/json'
@@ -87,37 +88,35 @@ function getUserOptions(tkn) {
 }
 
 function getUserInfo(req,res){
-	// if (((req.query.code)||state.applicationConfig.clientID)
-	//     &&   (req.query.code == state.applicationConfig.clientID))
-	// {
-	request(getUserOptions(state.auth.token), (err,httpResponse,body) => {
+	if (state.auth.token)
+	{
+		request(getUserOptions(state.auth.token), (err,httpResponse,body) => {
 		if (err){return console.error('err:', err)}
 		let response = JSON.parse(body);
 		response.token = !(empty(state.auth.token));
-		console.log("my info: \n", response);
 		res.render('response', {resp: response});
 		});
 	}
-	// else getAcceptance(res);
+	else getAcceptance(res);}
 
 
 function getToken(req,res){
-	//PUT /me/drive/root:/FolderA/FileB.txt:/content
-	// Content-Type: text/plain
-	if (((req.query.code)||state.applicationConfig.clientID)
-	    &&   (req.query.code == state.applicationConfig.clientID))
+	// check to see if token already exists and clientcode
+	// different from the one we're getting
+	if (((req.query.code)||state.auth.clientcode)
+	    || (!state.auth.token))
 	{
 		self=this;
 		console.log("now having to get token with code: ", self.state.applicationConfig.clientID);
-		request(buildTokenRequest(self.state.applicationConfig.clientID),
+		request(buildTokenRequest(self.state.auth.clientcode),
 			(err, httpResponse, body) => {
 			if (err) {
 			return console.error('upload failed:', err);
 			}
 			let response = JSON.parse(body);
 			if ((httpResponse == 400)&&(response.error=='invalid_grant'))
-				console.log("what ?! : ", this.state.applicationConfig.clientID);
-				this.state.applicationConfig.clientID = null;
+				console.log("what ?! : ", this.state.auth.clientcode);
+				this.state.auth.clientcode = null;
 			state.auth.token=response.access_token;
 			res.redirect('/');
 		})
@@ -131,7 +130,7 @@ function getToken(req,res){
 
 function uploadToDrive(req,res){
 	// make sure we still have a token
-	if ((!state.auth.token)||(state.applicationConfig.clientID))
+	if ((!state.auth.token)||(state.auth.clientcode))
 		res.redirect('/gettoken?code='+state.applicationConfig.clientID);
 	else if (!state.auth.token) res.redirect('/gettoken');
 	tkn=state.auth.token;
@@ -149,6 +148,8 @@ function uploadToDrive(req,res){
 	}
 	state.filename = req.file.originalname;
 	request.put(buildReq, (err,httpResponse,body) => {
+		//PUT /me/drive/root:/FolderA/FileB.txt:/content
+		// Content-Type: text/plain
 		if (err){return console.error('PUT error is :', err)}
 		info = JSON.parse(body)
 		uploaded = info.id;
